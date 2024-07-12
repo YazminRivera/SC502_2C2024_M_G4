02aee542afeb9eb0a267c1a1280a49344125caee
su <suarias0905@gmail.com>
2024-07-12 17:05:28 -0600

    Base adopciones
    Una pequeña base de adopciones con cards

---
+++ views/adopciones.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/CSS.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<h1 class="text-center">Adopciones</h1>
+      <br>
+      <div class="row">
+        <div class="col-1">  
+<section class="container" id="animales">                        
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>              
+            </div>
+          </div>  
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>              
+            </div>
+          </div>  
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://cdn.britannica.com/13/234213-050-45F47984/dachshund-dog.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>              
+            </div>
+          </div>  
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.thesprucepets.com/thmb/SV1nJpvIHP0bgP0JkD8GUFgMeZ8=/3600x0/filters:no_upscale():strip_icc()/white-dog-breeds-4846529-hero-4bf084c93c5b45a292e30559bb7e50e8.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>  
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://img.goodfon.com/wallpaper/big/f/5f/malamut-schenok-sobaka.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>  
+        </div>
+        <div class="col-1">          
+            </div>
+          </div>  
+        </div>
+      </div>
+    </section>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
