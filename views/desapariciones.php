8733ba75ab4f00cfed43ee4604cca4ae77c1501f
su <suarias0905@gmail.com>
2024-07-12 23:41:07 -0600

    Base desapariciones
    Se hicieron las cards de desapariciones con los datos correspondientes.

--- views/desapariciones.php
+++ views/desapariciones.php
@@ -4,10 +4,181 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/CSS.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+
+<h1 class="text-center">Desapariciones</h1>
+
+<section id="empleados">    
+<div class="row mt-3" >
+        <div class="col-1">            
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-07/Dachshund-Miniature-Smooth-Haired.jpg?itok=zFv4D76f" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Canela</h5>
+              <p class="card-text text-center mt-3">Raza: Dachshund</p>
+              <p class="card-text text-center">Edad: </p>  
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://i.pinimg.com/564x/87/29/59/87295912d6f0d913930fc37dfcc0e31c.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Cookie</h5>
+              <p class="card-text text-center mt-3">Raza: Husky</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://hips.hearstapps.com/hmg-prod/images/small-fluffy-dog-breeds-maltipoo-663009b6293cc.jpg?crop=0.668xw:1.00xh;0.143xw,0&resize=980:*" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Luffy</h5>
+              <p class="card-text text-center mt-3">Raza: Maltipoo</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+        
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Jack</h5>
+              <p class="card-text text-center mt-3">Raza: Pug</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.thesprucepets.com/thmb/ZVZtMyoEjEGtXIiwK2nxoQtsG5U=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/shih-tzu-dog-lying-on-a-newspaper-476683903-5816337c3df78cc2e891b5fd.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Armin</h5>
+              <p class="card-text text-center mt-3">Raza: Shih Tzu</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col-1">          
+            </div>
+          </div>  
+        </div>
+      </div>
+      <div class="row mt-3" >
+        <div class="col-1">            
+        </div>
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/68/Sun_Dog_Dalmatian.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Benito</h5>
+              <p class="card-text text-center mt-3">Raza: Dálmata</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.akc.org/wp-content/uploads/2017/11/German-Shepherd-on-White-00.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Sea</h5>
+              <p class="card-text text-center mt-3">Raza: Pastor Alemán</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bulldog_inglese.jpg/1200px-Bulldog_inglese.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Princesa</h5>
+              <p class="card-text text-center mt-3">Raza: Bulldog</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+        
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-09golden20retriever.jpg?itok=48StbVfe" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Goldie</h5>
+              <p class="card-text text-center mt-3">Raza: Golden Retriever</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div class="col">
+          <div class="card mb-3">
+            <img src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Max</h5>
+              <p class="card-text text-center mt-3">Raza: Beagle</p>
+              <p class="card-text text-center">Edad: </p>
+              <p class="card-text text-center">Ubicación perdida: </p>  
+              <div class="text-center">       
+              <button type="button" class="btn btn-success">Contactar</button>
+              </div>           
+            </div>
+          </div>  
+        </div>
+
+        <div class="col-1">          
+            </div>
+          </div>  
+        </div>
+      </div>      
+</section>   
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
