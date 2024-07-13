0f9d776ed9f177f9ca17e35c90d74872377a755c
Densma5 <dennysu05@gmail.com>
2024-07-13 16:38:41 -0600

    Se agrega menu.php junto a su menu.css y se hace el llamado de menu.php a la base de index.php

---
+++ views/menu.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="./assets/css/menu.css">
+<div class="seccion1">
+  <div class="container-fluid">
+    <div class="d-flex justify-content-between align-items-center">
+      <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/5871/5871586.png" alt="Logo" style="height: 80px;"></a>
+      <div>
+        <a href="#" class="btn btnInicio">Iniciar Sesión</a>
+        <a href="#" class="btn btnRegistro">Registrarse</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="seccion2">
+<nav class="navbar navbar-expand-lg bottom-bar">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Encontrados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Desapariciones</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Adopciones</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Información Castraciones</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Solicitar Apoyo</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+</nav>
