d586446bcbe38413bfac84b15390b67a99533f82
su <suarias0905@gmail.com>
2024-08-10 23:50:55 -0600

    cambios en controller y js
    se agregaron algunas cosas al controller de solicitudadopcion y un cambio en el url en el js

--- controllers/solicitudAdopcionController.php
+++ controllers/solicitudAdopcionController.php
@@ -1,6 +1,24 @@
 <?php
 require_once '../models/solicitudAdopcionModel.php';
 
+$nombreSoliAdopcion = (isset($_POST["nombreSoliAdopcion"])) ? $_POST["nombreSoliAdopcion"] : "";
+$correoSoliAdopcion = (isset($_POST["correoSoliAdopcion"])) ? $_POST["correoSoliAdopcion"] : "";
+$telefonoSoliAdopcion = (isset($_POST["telefonoSoliAdopcion"])) ? $_POST["telefonoSoliAdopcion"] : "";
+$direccionSoliAdopcion = (isset($_POST["direccionSoliAdopcion"])) ? $_POST["direccionSoliAdopcion"] : "";
 
+$solicitud = new solicitudAdopcionModel();
+$solicitud-> setNombre($nombreSoliAdopcion); 
+$solicitud-> setCorreo($correoSoliAdopcion); 
+$solicitud-> setTelefono($telefonoSoliAdopcion); 
+$solicitud-> setDireccion($direccionSoliAdopcion); 
+
+try {
+    $solicitud->guardar(); //cambiar
+    $resp = array("exito" => true, "msg" => "Registro realizado con éxito a la BD");
+    echo json_encode($resp);
+} catch (PDOException $th) {
+    $resp = array("exito" => false, "msg" => "Se presentó un error");
+    echo json_encode($resp);
+}
 
 ?>
