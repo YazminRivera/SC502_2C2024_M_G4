8d6cf7bd9707d9264f7073153aa662568cbfe9c2
Yazmin <yrivera40037@ufide.ac.cr>
2024-08-20 18:45:57 -0600

    se corrigio error de id usuario de solicitud de apoyo si la persona no esta logueada

--- views/solicitud_apoyo.php
+++ views/solicitud_apoyo.php
@@ -83,7 +83,12 @@
                         <label for="Ubicación" class="form-label">Ubicación</label>
                         <div id="map" class="mapa"></div>
                     </div>
-                    <input type="hidden" id="idUsuario" name="idUsuario" value="<?php echo $_SESSION['user']['idUsuario']?>">
+                    <input type="hidden" id="idUsuario" name="idUsuario" 
+                    value="<?php if (!isset($_SESSION['user'])) {
+                            echo '';
+                        } else {
+                            echo $_SESSION['user']['idUsuario'];
+                        }?>">
                     <div class="col-12 mt-3 boton-end">
                         <button class="btn-Enviar" type="submit">Enviar</button>
                     </div>
